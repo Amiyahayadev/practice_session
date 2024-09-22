d8df124e71cb7edd4428c6ebef23155f73854294
Amiyahayadev <ameenahyahayab@gmail.com>
2024-09-22 17:24:05 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// alert("Welcome!");
+// getUserDetails();
+// alert("Sign out is successful");
+
+
+// function getUserDetails() {
+//     let userName = prompt("Please enter your username: ")
+
+//     while (!validateUserName(userName)) {
+//         userName = prompt("Error - Enter the correct username: ")
+//     }
+
+// }
+
+// function validateUserName(userName) {
+//     if (userName == null) {
+// 		console.log("User cancelled");
+// 		return true;
+//     }
+
+//     if (userName.length > 10) {
+//         alert("Username cannot be more than 10 characters long");
+//         return false;
+//     } else
+//     return true;
+
+// }
