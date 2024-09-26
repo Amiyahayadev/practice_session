45e655a2609192df91e590c6d374e252031163e7
Amiyahayadev <ameenahyahayab@gmail.com>
2024-09-26 15:58:00 +0100

    add basic files and an external package called 'confetti'

---
+++ npm-for-beginners/index.js
@@ -0,0 +1,13 @@
+// import JSConfetti from "js-confetti";
+// JSConfetti is a global variable
+
+console.log(JSConfetti);  // this line imports the external js package
+
+const jsConfetti = new JSConfetti(); // initalize an instance of JSConfetti
+
+jsConfetti.addConfetti();            //call addConfetti method to run it!
+
+/*
+----------------------------------------------------------------
+*/
+console.log("I am a Js Queen!");
