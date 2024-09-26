5b99496cc568721349620b4d4b1d4287087d7f47
Amiyahayadev <ameenahyahayab@gmail.com>
2024-09-26 14:38:38 +0100

    coding practice session(1)

---
+++ codeacademy.js
@@ -0,0 +1,46 @@
+/*
+ * This is a
+ * Multiline comment in javascript
+ */
+
+// This is a single line comment in javascript
+
+const WORD = "codecademy";
+console.log(WORD.length);
+
+const GOAL = `learn web developement`;
+console.log(`One does not simply ${GOAL}`);
+
+class Rectangle {
+	constructor(height, width) {
+		this.height = height;
+		this.width = width;
+	}
+}
+const square = new Rectangle(2, 2);
+
+let primaryCandidate = null;
+
+const number = 7;
+const dayInAWeek = number;
+const colorsOfRainbow = number;
+
+Math.random();
+
+let service = "credit";
+let month = "May 30th";
+let displayText = "Your " + service + " bill is due on " + month + ".";
+
+console.log(displayText);
+
+// Assignment operators
+let number$ = 100;
+number$ = number$ + 10;
+number$ += 10;
+console.log(number$);
+
+number$ -= 100
+console.log(number$)
+
+var a;
+console.log(a)
