db01e5d49f726ade190a85a0944a07d1486a5da5
Amiyahayadev <ameenahyahayab@gmail.com>
2024-11-04 11:38:28 +0100

    moday coding session

--- codeacad.js
+++ codeacad.js
@@ -89,3 +89,67 @@ firstCharacterUpper + restOfString;
 let sillyString = "hELlo THERE, hOW ARE yOu doINg?";
 sillyString[0].toUpperCase() + sillyString.slice(1).toLowerCase();
 ("Hello there, how are you doing?");
+
+////////////////////////////////////////
+// let the rider be allowed to pass if they're atleast 60 inches tall
+// IF riderHeight is GREATER THAN or EQUAL to 60 inches
+
+let riderHeight = 60;
+let heightRestriction = 60;
+riderHeight >= heightRestriction;
+true
+// let the rider be allowed to pass if they're no more than 48 inches tall
+// IF riderHeight is LESS THAN or EQUAL to 48 inches
+undefined
+riderHeight = 48;
+heightRestriction = 48;
+riderHeight <= heightRestriction;
+true
+riderHeight = 50;
+heightRestriction = 48;
+riderHeight <= heightRestriction;
+false
+let mySecretNumber = 5;
+
+let chicoGuess = 3;
+mySecretNumber === chicoGuess;
+false
+let harpoGuess = 7;
+mySecretNumber === harpoGuess;
+false
+let grouchoGuess = 5;
+mySecretNumber === grouchoGuess;
+true
+
+let stringNumber = "5";
+let actualNumber = 5;
+stringNumber == actualNumber;
+true
+0 == false
+true
+0 == "false"
+false
+false = "false"
+
+false
+1 == true
+true
+0 == false
+true
+0 == '0'
+true
+// Movie time: if someone is 13 or over, they’re allowed in. If they’re not over 13, but with an adult company, they’re also allowed in. Otherwise, they can’t see the movie.
+
+
+
+true
+// Movie time: if someone is 13 or over, they’re allowed in. If they’re not over 13, but with an adult company, they’re also allowed in. Otherwise, they can’t see the movie.
+let age = 12;
+let accompanied = true;
+if (age >= 13) {
+    console.log("Allowed in");
+} else if (age < 13 && accompanied) {
+    console.log("Allowed in too.");
+} else {
+    console.log("Not allowed");
+}
